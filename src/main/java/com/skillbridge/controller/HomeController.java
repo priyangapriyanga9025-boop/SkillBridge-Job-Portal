495f115821e9e3d910b9bd911b9647d4245e97fc
priyangapriyanga9025-boop <priyangapriyanga9025@gmail.com>
2026-07-16 13:41:05 +0530

    Fix home redirect

--- src/main/java/com/skillbridge/controller/HomeController.java
+++ src/main/java/com/skillbridge/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.skillbridge.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
-        @GetMapping("/")
-    public String home(){
+
+    @GetMapping("/")
+    public String home() {
         return "redirect:/login.html";
     }
 }
